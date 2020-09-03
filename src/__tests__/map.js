20bafdb19dfe7068ac68c0225a80c79cf4b424f0
Mohammed Samir <ms.2052001@gmail.com>
2020-09-03 18:43:48 +0200

    Add some tests for options.function

--- src/__tests__/map.js
+++ src/__tests__/map.js
@@ -1,3 +1,4 @@
+
 let tests = {
   basic: [
     {
@@ -59,8 +60,110 @@ let tests = {
           },
         ],
       },
-    },
+    }
   ],
+  singleChar: {
+    autoMult: [
+      {
+        tex: '3^6cd\\sqrt af',
+        struct: {
+          "type": "automult",
+          "args": [
+            {
+              "type": "automult",
+              "args": [
+                {
+                  "type": "automult",
+                  "args": [
+                    {
+                      "type": "automult",
+                      "args": [
+                        {
+                          type: "operator", name: "^",
+                          args: [ 3, 6 ]
+                        },
+                        "c"
+                      ]
+                    },
+                    "d"
+                  ]
+                },
+                { type: "sqrt", args: ["a"] }
+              ]
+            },
+            "f"
+          ]
+        }
+      },
+      {
+        tex: '12+3^6x \\frac 1 {5+3}',
+        struct: {
+          "name": "+", "type": "operator",
+          "args": [
+            12,
+            {
+              "type": "automult",
+              "args": [
+                {
+                  "type": "automult",
+                  "args": [
+                    {
+                      "name": "^", "type": "operator",
+                      "args": [ 3, 6 ]
+                    },
+                    "x"
+                  ]
+                },
+                {
+                  "type": "frac",
+                  "args": [
+                    1,
+                    {
+                      "name": "+", "type": "operator",
+                      "args": [ 5, 3 ]
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      }
+    ]
+  },
+  multiChar: {
+    autoMult: [
+
+    ],
+  },
+
+  options: {
+    functions: [
+      {
+        tex: `f(x)`,
+        parseOptions: { functions: 'f' },
+        struct: {
+          type: 'function', name: "f",
+          args: [{
+            type: 'block', name: "()",
+            args: ["x"]
+          }]
+        },
+      },
+      {
+        tex: `f\\left(x\\right)`,
+        parseOptions: { functions: 'f' },
+        struct: {
+          type: 'function', name: "f",
+          args: [{
+            type: 'block', name: "\\left(\\right)",
+            args: ["x"]
+          }]
+        }
+      }
+    ]
+  }
+
 };
 
 module.exports = tests;
