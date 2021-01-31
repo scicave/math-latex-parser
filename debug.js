fec87ee507dc050484018714f0eeb80a89ec443b
Mohammed Samir <ms.2052001@gmail.com>
2021-01-31 14:16:03 +0200

    Update debug.js script

--- debug.js
+++ debug.js
@@ -1,13 +1,22 @@
-const texParser = require('./lib/');
+// const texParser = require('./lib/');
 const prepareInput = require('./src/prepareInput.js');
-let tex = process.env.tex || `a+ \\frac 1 {5+3} \\int 12`;
+let tex = process.env.tex || `a+ \\left {5+3} `;
 
 function parse() {
   console.log('parsing:', tex);
+  return texParser.parse(tex);
+}
+
+function log(fn, ...args){
+  console.log('start >>>>>>>>>>>>>>');
+  console.log('===============');
+  console.log(tex);
+  console.log('===============');
+  console.time(fn.name + ' is done after');
   try {
-    return texParser.parse(tex);
+    console.log(fn(...args));
   } catch (e) {
-    if (e instanceof texParser.SyntaxError) {
+    if (e instanceof SyntaxError || e instanceof texParser.SyntaxError) {
       console.log('SyntaxError:', e.message);
 
       const i = e.location.start.line - 1;
@@ -25,19 +34,82 @@ function parse() {
       throw e;
     }
   }
+  console.log('===============');
+  console.timeEnd(fn.name + ' is done after');
 }
 
-function log(fn, ...args){
-  console.log('start >>>>>>>>>>>>>>');
-  console.log('===============');
-  console.log(tex);
-  console.log('===============');
-  console.time(prepareInput.name + ' is done after');
-  console.log(fn(...args));
-  console.log('===============');
-  console.timeEnd(prepareInput.name + ' is done after');
+// ---------------------------------
+//         PEGjs polyfill
+// ---------------------------------
+
+let peg$posDetailsCache  = [{ line: 1, column: 1 }];
+
+function peg$computePosDetails(pos) {
+  var details = peg$posDetailsCache[pos], p;
+
+  if (details) {
+    return details;
+  } else {
+    p = pos - 1;
+    while (!peg$posDetailsCache[p]) {
+      p--;
+    }
+
+    details = peg$posDetailsCache[p];
+    details = {
+      line:   details.line,
+      column: details.column
+    };
+
+    while (p < pos) {
+      if (tex.charCodeAt(p) === 10) {
+        details.line++;
+        details.column = 1;
+      } else {
+        details.column++;
+      }
+
+      p++;
+    }
+
+    peg$posDetailsCache[pos] = details;
+    return details;
+  }
 }
 
-log(parse, tex);
+function peg$computeLocation(startPos, endPos) {
+  var startPosDetails = peg$computePosDetails(startPos),
+      endPosDetails   = peg$computePosDetails(endPos);
 
-// log();
+  return {
+    start: {
+      offset: startPos,
+      line:   startPosDetails.line,
+      column: startPosDetails.column
+    },
+    end: {
+      offset: endPos,
+      line:   endPosDetails.line,
+      column: endPosDetails.column
+    }
+  };
+}
+
+function error(message, location) {
+  location = location !== void 0 ? location : peg$computeLocation(peg$savedPos, peg$currPos)
+  throw new SyntaxError(message, location);
+}
+
+class SyntaxError extends Error {
+  constructor (msg, location) {
+    super(msg);
+    this.location = location;
+  }
+}
+
+// -----------------------------------
+//   finnally excute the desire
+//   function with the desired args
+// -----------------------------------
+
+log(prepareInput, tex, peg$computeLocation, error);
